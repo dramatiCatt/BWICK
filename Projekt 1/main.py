cefa426d7e17114ad4af17c70607e2838a41dcb5
MAIPA01 <marcelipatryk@gmail.com>
2025-06-30 10:24:28 +0200

    Update main.py

--- Projekt 1/main.py
+++ Projekt 1/main.py
@@ -74,7 +74,7 @@ def test_authentication():
     AUTHENTICATION_THRESHOLD = 0.4
     AUTHENTICATION_NUM = 20
 
-    truth_table = np.zeros(shape=(DATATSETS_NUM * FINGERPRINTS_NUM * TEST_IMG_PER_FINGERPRINT, FINGERPRINTS_NUM), dtype=bool)
+    truth_table = np.zeros(shape=(DATATSETS_NUM, FINGERPRINTS_NUM * TEST_IMG_PER_FINGERPRINT, FINGERPRINTS_NUM), dtype=bool)
     result_table = np.zeros_like(truth_table)
 
     for dataset_idx, dataset_test_fingerprints_paths in enumerate(test_fingerprints_data_paths):       
@@ -93,41 +93,64 @@ def test_authentication():
 
                         print(f"Test {test_idx + 1} of Fingerprint {fingerprint_idx + 1} from dataset {dataset_idx + 1} on Fingerprint {collection_idx + 1} from dataset {collection_dataset_idx + 1}: {'Authenticated' if is_good else 'Not authenticated'} in {end - start:.6f} seconds")
 
-                        table_idx = (dataset_idx * FINGERPRINTS_NUM + fingerprint_idx) * TEST_IMG_PER_FINGERPRINT + test_idx
-                        if collection_idx == fingerprint_idx:
-                            truth_table[table_idx, collection_idx] = True
-                        else:
-                            truth_table[table_idx, collection_idx] = False
+                        table_idx = fingerprint_idx * TEST_IMG_PER_FINGERPRINT + test_idx
 
-                        result_table[table_idx, collection_idx] = is_good
+                        truth_table[dataset_idx, table_idx, collection_idx] = (collection_idx == fingerprint_idx)
+                        result_table[dataset_idx, table_idx, collection_idx] = is_good
 
-    truth_table = truth_table.flatten()
+    all_true_positive_num = 0
+    all_true_negative_num = 0
+    all_false_positive_num = 0
+    all_false_negative_num = 0
+    for dataset_idx in range(DATATSETS_NUM):
+        dataset_truth_table = truth_table[dataset_idx].flatten()
+        dataset_result_table = result_table[dataset_idx].flatten()
+
+        # print("Classification Stats:")
+        # print(skl.metrics.classification_report(dataset_truth_table, dataset_result_table, labels=[False, True], digits=4, zero_division=0.0))
+
+        true_false_table = dataset_truth_table == dataset_result_table
+        true_mask = np.argwhere(true_false_table)
+
+        true_table = dataset_result_table[true_mask]
+        true_positive_num = np.count_nonzero(true_table)
+        true_negative_num = len(true_table) - true_positive_num
+
+        false_positive_num = np.count_nonzero(dataset_result_table) - true_positive_num
+        false_negative_num = len(dataset_result_table) - false_positive_num - true_positive_num - true_negative_num
+
+        all_true_positive_num += true_positive_num
+        all_true_negative_num += true_negative_num
+        all_false_positive_num += false_positive_num
+        all_false_negative_num += false_negative_num
+
+        accuracy = 0.0 if len(dataset_result_table) == 0 else (true_positive_num + true_negative_num) / len(dataset_result_table)
+
+        print()
+        print(f"Dataset {dataset_idx + 1}")
+        print(f"Results num: {len(result_table)}")
+        print(f"True Positive: {true_positive_num}, expected: {FINGERPRINTS_NUM * TEST_IMG_PER_FINGERPRINT}")
+        print(f"True Negative: {true_negative_num}, expected: {(FINGERPRINTS_NUM ** 2 - FINGERPRINTS_NUM) * TEST_IMG_PER_FINGERPRINT}")
+        print(f"False Positive: {false_positive_num}, expected: 0")
+        print(f"False Negative: {false_negative_num}, expected: 0")
+        print(f"False Accept Rate (FAR): {false_positive_num / (false_positive_num + true_negative_num)}")
+        print(f"False Reject Rate (FRR): {false_negative_num / (false_negative_num + true_positive_num)}")
+        print(f"Genuine Accept Rate (GAR): {true_positive_num / (true_positive_num + false_negative_num)}")
+        print(f"Accuracy: {accuracy * 100.0} %")
+
     result_table = result_table.flatten()
-
-    # print("Classification Stats:")
-    # print(skl.metrics.classification_report(truth_table, result_table, labels=[False, True], digits=4, zero_division=0.0))
-
-    true_false_table = truth_table == result_table
-    true_mask = np.argwhere(true_false_table)
-
-    true_table = result_table[true_mask]
-    true_positive_num = np.count_nonzero(true_table)
-    true_negative_num = len(true_table) - true_positive_num
-
-    false_positive_num = np.count_nonzero(result_table) - true_positive_num
-    false_negative_num = len(result_table) - false_positive_num - true_positive_num - true_negative_num
-
-    accuracy = 0.0 if len(result_table) == 0 else (true_positive_num + true_negative_num) / len(result_table)
+    accuracy = 0.0 if len(result_table) == 0 else (all_true_positive_num + all_true_negative_num) / len(result_table)
 
     print()
+    print("All Datasets")
     print(f"Results num: {len(result_table)}")
-    print(f"True Positive: {true_positive_num}, expected: {DATATSETS_NUM * FINGERPRINTS_NUM * TEST_IMG_PER_FINGERPRINT}")
-    print(f"True Negative: {true_negative_num}, expected: {(FINGERPRINTS_NUM ** 2 - FINGERPRINTS_NUM) * TEST_IMG_PER_FINGERPRINT * DATATSETS_NUM}")
-    print(f"False Positive: {false_positive_num}, expected: 0")
-    print(f"False Negative: {false_negative_num}, expected: 0")
-    print(f"False Accept Rate (FAR): {false_positive_num / (false_positive_num + true_negative_num)}")
-    print(f"False Reject Rate (FRR): {false_negative_num / (false_negative_num + true_positive_num)}")
-    print(f"Genuine Accept Rate (GAR): {true_positive_num / (true_positive_num + false_negative_num)}")
+    print(f"True Positive: {all_true_positive_num}, expected: {DATATSETS_NUM * FINGERPRINTS_NUM * TEST_IMG_PER_FINGERPRINT}")
+    print(f"True Negative: {all_true_negative_num}, expected: {(FINGERPRINTS_NUM ** 2 - FINGERPRINTS_NUM) * TEST_IMG_PER_FINGERPRINT * DATATSETS_NUM}")
+    print(f"False Positive: {all_false_positive_num}, expected: 0")
+    print(f"False Negative: {all_false_negative_num}, expected: 0")
+    print(f"False Accept Rate (FAR): {all_false_positive_num / (all_false_positive_num + all_true_negative_num)}")
+    print(f"False Reject Rate (FRR): {all_false_negative_num / (all_false_negative_num + all_true_positive_num)}")
+    print(f"Genuine Accept Rate (GAR): {all_true_positive_num / (all_true_positive_num + all_false_negative_num)}")
     print(f"Accuracy: {accuracy * 100.0} %")
 
 def main() -> None:
